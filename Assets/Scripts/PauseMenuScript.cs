c0b35ae3243cf56020072e052c6ada0cfcb3c0af
Justin Thibodeau <jgthibodeau@umass.edu>
2015-08-19 16:33:45 -0400

    Deleting files, finalizing pause menu

--- Assets/Scripts/PauseMenuScript.cs
+++ Assets/Scripts/PauseMenuScript.cs
@@ -1,14 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PauseScript : MonoBehaviour {
+public class PauseMenuScript : MonoBehaviour {
 	public GameObject pauseMenu;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	public void Update(){
 		if (GlobalScript.GetButton ("Pause")) {
@@ -28,5 +23,12 @@ public class PauseScript : MonoBehaviour {
 		GlobalScript.currentGameState = GlobalScript.GameState.InGame;
 		Time.timeScale = 1f;
 		pauseMenu.SetActive (false);
+	}
+	public void Quit(){
+		Application.Quit();
+	}
+	public void MainMenu(){
+		//TODO
+		//Application.LoadLevel("MainMenu");
 	}
 }
